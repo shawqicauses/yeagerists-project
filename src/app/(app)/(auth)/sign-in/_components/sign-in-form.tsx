87457ae5d49dbb4d2f0fa132b1e57de63e48cbe1
Shawqi Hatem - Developer and Instructor <shawqicauses@outlook.com>
2025-09-13 15:53:23 +0200

    New: Enhance through adding sign-in functionalities

--- src/app/(app)/(auth)/sign-in/_components/sign-in-form.tsx
+++ src/app/(app)/(auth)/sign-in/_components/sign-in-form.tsx
@@ -1,86 +1,114 @@
-// REVIEWED
+// REVIEWED - 01
 
 "use client";
 
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useAuth } from "@/hooks/use-auth";
+import { SignInSchema, signInSchema } from "@/lib/schemas/auth";
 
 import { RoleOption, rolesOptions } from "./role-option";
 
 export const SignInForm = function SignInForm() {
+  const { signIn } = useAuth();
+
   const [roleSelected, setRoleSelected] = useState<string>(rolesOptions[0].id);
 
+  const form = useForm<SignInSchema>({
+    mode: "onBlur",
+    resolver: zodResolver(signInSchema),
+    defaultValues: {
+      email: "sample@example.com",
+      password: "SamplePassword@1234",
+    },
+  });
+
+  const handleSubmit = function handleSubmit(data: SignInSchema) {
+    toast.loading("Signing in...", {
+      id: "sign-in",
+    });
+
+    signIn.mutate({
+      email: data.email,
+      password: data.password,
+    });
+  };
+
   return (
-    <form className="space-y-6">
-      <fieldset>
-        <legend className="text-sm/6 font-semibold text-foreground">
-          Select your role
-        </legend>
-        <div className="mt-6 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-4">
-          {rolesOptions.map((roleOption) => (
-            <RoleOption
-              key={roleOption.id}
-              roleOption={roleOption}
-              roleSelect={setRoleSelected}
-              roleSelected={roleSelected}
-            />
-          ))}
-        </div>
-      </fieldset>
-
-      <div>
-        {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-        <label
-          htmlFor="email"
-          className="block text-sm/6 font-medium text-foreground">
-          Email address
-        </label>
-        <div className="mt-2">
-          <input
-            id="email"
-            name="email"
-            type="email"
-            autoComplete="email"
-            required
-            className="focus:outline-tertiary-2 block w-full rounded-md bg-background px-3 py-1.5 text-base text-foreground outline outline-1 -outline-offset-1 outline-input placeholder:text-muted-foreground focus:outline focus:outline-2 focus:-outline-offset-2 sm:text-sm/6"
-          />
-        </div>
-      </div>
-
-      <div>
-        <div className="flex items-center justify-between">
-          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-          <label
-            htmlFor="password"
-            className="block text-sm/6 font-medium text-foreground">
-            Password
-          </label>
-          <div className="text-sm">
-            <a
-              href="/password/reset"
-              className="text-tertiary-2 font-semibold hover:text-foreground">
-              Forgot password?
-            </a>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(handleSubmit)} className="space-y-6">
+        <fieldset>
+          <legend className="text-sm/6 font-semibold text-foreground">
+            Select your role
+          </legend>
+          <div className="mt-6 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-4">
+            {rolesOptions.map((roleOption) => (
+              <RoleOption
+                key={roleOption.id}
+                roleOption={roleOption}
+                roleSelect={setRoleSelected}
+                roleSelected={roleSelected}
+              />
+            ))}
           </div>
-        </div>
-        <div className="mt-2">
-          <input
-            id="password"
-            name="password"
-            type="password"
-            autoComplete="current-password"
-            required
-            className="focus:outline-tertiary-2 block w-full rounded-md bg-background px-3 py-1.5 text-base text-foreground outline outline-1 -outline-offset-1 outline-input placeholder:text-muted-foreground focus:outline focus:outline-2 focus:-outline-offset-2 sm:text-sm/6"
-          />
-        </div>
-      </div>
+        </fieldset>
 
-      <div>
-        <button
-          type="submit"
-          className="bg-tertiary-2 focus-visible:outline-tertiary-2 flex w-full justify-center rounded-md px-3 py-1.5 text-sm/6 font-semibold text-background shadow-sm hover:bg-foreground focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2">
-          Sign in
-        </button>
-      </div>
-    </form>
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email address</FormLabel>
+              <FormControl>
+                <Input {...field} type="email" />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <div className="flex items-center justify-between">
+                <FormLabel>Password</FormLabel>
+                <div className="text-sm">
+                  <a
+                    href="/password/reset"
+                    className="font-semibold text-tertiary-2 hover:text-foreground">
+                    Forgot password?
+                  </a>
+                </div>
+              </div>
+              <FormControl>
+                <Input {...field} type="password" />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <div>
+          <Button variant="tertiary-2" disabled={signIn.isPending}>
+            {signIn.isPending ? "Signing in..." : "Sign in"}
+          </Button>
+        </div>
+      </form>
+    </Form>
   );
 };
