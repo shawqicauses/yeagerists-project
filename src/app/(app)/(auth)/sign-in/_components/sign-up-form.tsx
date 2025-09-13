a181427901e7920464dfc6a17bb22f0d49bd9f83
Shawqi Hatem - Developer and Instructor <shawqicauses@outlook.com>
2025-09-13 15:44:56 +0200

    New: Enhance through adding sign-up functionalities

--- src/app/(app)/(auth)/sign-in/_components/sign-up-form.tsx
+++ src/app/(app)/(auth)/sign-in/_components/sign-up-form.tsx
@@ -1,115 +1,148 @@
-// REVIEWED
+// REVIEWED - 01
 
 "use client";
 
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useAuth } from "@/hooks/use-auth";
+import { signUpSchema, SignUpSchema } from "@/lib/schemas/auth";
 
 import { RoleOption, rolesOptions } from "./role-option";
 
 export const SignUpForm = function SignUpForm() {
+  const { signUp } = useAuth();
+
   const [roleSelected, setRoleSelected] = useState<string>(rolesOptions[0].id);
 
+  const form = useForm<SignUpSchema>({
+    mode: "onBlur",
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
+      role: roleSelected,
+      name: "Sample Name",
+      email: "sample@example.com",
+      password: "SamplePassword@1234",
+    },
+  });
+
+  const handleSubmit = function handleSubmit(data: SignUpSchema) {
+    toast.loading("Signing up...", {
+      id: "sign-up",
+    });
+
+    signUp.mutate({
+      role: data.role,
+      name: data.name,
+      email: data.email.trim().toLowerCase(),
+      password: data.password,
+    });
+  };
+
   return (
-    <form className="space-y-6">
-      <fieldset>
-        <legend className="text-sm/6 font-semibold text-foreground">
-          Select your role
-        </legend>
-        <div className="mt-6 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-4">
-          {rolesOptions.map((roleOption) => (
-            <RoleOption
-              key={roleOption.id}
-              roleOption={roleOption}
-              roleSelect={setRoleSelected}
-              roleSelected={roleSelected}
-            />
-          ))}
-        </div>
-      </fieldset>
-
-      {roleSelected === "issuer-user" ? (
-        <div>
-          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-          <label
-            htmlFor="issuer-name"
-            className="block text-sm/6 font-medium text-foreground">
-            Issuer Name
-          </label>
-          <div className="mt-2">
-            <input
-              id="issuer-name"
-              name="issuer-name"
-              type="text"
-              required
-              className="focus:outline-tertiary-2 block w-full rounded-md bg-background px-3 py-1.5 text-base text-foreground outline outline-1 -outline-offset-1 outline-input placeholder:text-muted-foreground focus:outline focus:outline-2 focus:-outline-offset-2 sm:text-sm/6"
-            />
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(handleSubmit)} className="space-y-6">
+        <fieldset>
+          <legend className="text-sm/6 font-semibold text-foreground">
+            Select your role
+          </legend>
+          <div className="mt-6 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-4">
+            {rolesOptions.map((roleOption) => (
+              <RoleOption
+                key={roleOption.id}
+                roleOption={roleOption}
+                roleSelect={setRoleSelected}
+                roleSelected={roleSelected}
+              />
+            ))}
           </div>
-        </div>
-      ) : (
+        </fieldset>
+
+        {roleSelected === "issuer-user" ? (
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+                <FormLabel className="block text-sm/6 font-medium text-foreground">
+                  Issuer Name
+                </FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        ) : (
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="block text-sm/6 font-medium text-foreground">
+                  Accredited User Name
+                </FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        )}
+
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="block text-sm/6 font-medium text-foreground">
+                Email address
+              </FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="block text-sm/6 font-medium text-foreground">
+                Password
+              </FormLabel>
+              <FormControl>
+                <Input {...field} type="password" />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
         <div>
-          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-          <label
-            htmlFor="accredited-user-name"
-            className="block text-sm/6 font-medium text-foreground">
-            Accredited User Name
-          </label>
-          <div className="mt-2">
-            <input
-              id="accredited-user-name"
-              name="accredited-user-name"
-              type="text"
-              required
-              className="focus:outline-tertiary-2 block w-full rounded-md bg-background px-3 py-1.5 text-base text-foreground outline outline-1 -outline-offset-1 outline-input placeholder:text-muted-foreground focus:outline focus:outline-2 focus:-outline-offset-2 sm:text-sm/6"
-            />
-          </div>
+          <Button variant="tertiary-2" disabled={signUp.isPending}>
+            {signUp.isPending ? "Signing up..." : "Sign up"}
+          </Button>
         </div>
-      )}
-
-      <div>
-        {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-        <label
-          htmlFor="email"
-          className="block text-sm/6 font-medium text-foreground">
-          Email address
-        </label>
-        <div className="mt-2">
-          <input
-            id="email"
-            name="email"
-            type="email"
-            autoComplete="email"
-            required
-            className="focus:outline-tertiary-2 block w-full rounded-md bg-background px-3 py-1.5 text-base text-foreground outline outline-1 -outline-offset-1 outline-input placeholder:text-muted-foreground focus:outline focus:outline-2 focus:-outline-offset-2 sm:text-sm/6"
-          />
-        </div>
-      </div>
-
-      <div>
-        {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-        <label
-          htmlFor="password"
-          className="block text-sm/6 font-medium text-foreground">
-          Password
-        </label>
-        <div className="mt-2">
-          <input
-            id="password"
-            name="password"
-            type="password"
-            autoComplete="current-password"
-            required
-            className="focus:outline-tertiary-2 block w-full rounded-md bg-background px-3 py-1.5 text-base text-foreground outline outline-1 -outline-offset-1 outline-input placeholder:text-muted-foreground focus:outline focus:outline-2 focus:-outline-offset-2 sm:text-sm/6"
-          />
-        </div>
-      </div>
-
-      <div>
-        <button
-          type="submit"
-          className="bg-tertiary-2 focus-visible:outline-tertiary-2 flex w-full justify-center rounded-md px-3 py-1.5 text-sm/6 font-semibold text-background shadow-sm hover:bg-foreground focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2">
-          Sign up
-        </button>
-      </div>
-    </form>
+      </form>
+    </Form>
   );
 };
